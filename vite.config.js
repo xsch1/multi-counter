59f4affb69df3b0517d3a98551994b775d0b0d49
xxschi <141630459+xxschi@users.noreply.github.com>
2025-08-14 17:19:32 +0900

    chore: flatten project to repo root and setup Pages workflow

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/multi-counter/', // 리포 이름과 동일하게 수정
+  base: '/multi-counter/', // 리포 이름과 동일하게
 });
